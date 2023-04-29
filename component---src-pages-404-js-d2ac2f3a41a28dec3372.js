356fb41149a632c3c7dbbff2b38827e060eabe48
zacbe <zacbegonzalez@gmail.com>
2023-04-29 15:20:19 +0200

    Updates

---
+++ component---src-pages-404-js-d2ac2f3a41a28dec3372.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkzacbe_github_io=self.webpackChunkzacbe_github_io||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),u=n(3544);t.default=()=>l.createElement(u.Z,null,l.createElement("div",{style:{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}},l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}]);
+//# sourceMappingURL=component---src-pages-404-js-d2ac2f3a41a28dec3372.js.map
